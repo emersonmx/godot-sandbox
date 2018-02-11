0f0f301a23181b57ade30b7d3cf1b74a8ad6f06d
Emerson MX <emersonmx@gmail.com>
2018-02-11 14:13:13 -0300

    Use method instead const

--- grid-move/grid.gd
+++ grid-move/grid.gd
@@ -3,10 +3,13 @@ extends TileMap
 const MARK = 0
 
 func mark_cell(position):
-    set_cellv(position, MARK)
+    set_cellv(position, _get_mark_tile_id())
 
 func clear_cell(position):
     set_cellv(position, INVALID_CELL)
 
 func is_cell_empty(position):
     return get_cellv(position) == INVALID_CELL
+
+func _get_mark_tile_id():
+    return 0
